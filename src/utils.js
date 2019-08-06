e75d272bb23847762333307c7a89d977b1c08999
Michael van Meerwijk <michaelvanm@gmail.com>
2019-08-06 11:34:23 +0200

    Random utilities

--- src/utils.js
+++ src/utils.js
@@ -18,3 +18,12 @@ const oppositeDirections = {
 export const oppositeDir = dir => {
 	return oppositeDirections[dir];
 }
+
+export const rnd = (maxExcl) => Math.floor(Math.random() * maxExcl);
+
+export const rndElement = arr => arr[rnd(arr.length)];
+
+export const rndElement2D = d2Arr => {
+	const row = d2Arr[rnd(d2Arr.length)];	
+	return row[rnd(row.length)];
+}
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,10 +1,59 @@
 import { DIR_NAMES } from '../src/constants';
-import { oppositeDir } from '../src/utils';
+import { oppositeDir, rnd, rndElement, rndElement2D } from '../src/utils';
 
 describe('getting the opposite direction', () => {
 	it('returns the correct direction', () => {
 		expect(oppositeDir(DIR_NAMES.N)).toBe(DIR_NAMES.S);
 		expect(oppositeDir(DIR_NAMES.S)).toBe(DIR_NAMES.N);
 		expect(oppositeDir(DIR_NAMES.E)).toBe(DIR_NAMES.W);
+	})
+})
+
+describe('randomize functions', () => {
+	describe('rnd', () => {
+		it('returns a random integer', () => {
+			let n = rnd(10);
+			expect(n).toEqual(parseInt(n));
+		})
+
+		it('returns a number from 0 up to (but not including) an argument', () => {
+			let rndNums = [];
+			for (let i = 0; i < 20; i++) {
+				rndNums.push(rnd(2));
+			}
+
+			expect(rndNums).toContain(0);
+			expect(rndNums).toContain(1);
+			expect(rndNums).not.toContain(2);
+		})
+	})
+
+	describe('rnd array element', () => {
+		it('returns a random element from an array', () => {
+			let arr = [1, 2, 3];
+
+			for (let i = 0; i < 10; i++) {
+				expect(arr).toContain(rndElement(arr));
+			}
+		})
+	})
+
+	describe('rnd 2d array element', () => {
+		it('returns a random item from a 2d array', () => {
+			let arr = [
+				['a', 'b'],
+				['c', 'd']
+			];
+			let items = ['a', 'b', 'c', 'd'];
+			let randomItems = new Set();
+	
+			for (let i = 0; i < 50; i++) {
+				randomItems.add(rndElement2D(arr));
+			}
+			
+			items.forEach(item => {
+				expect(randomItems.has(item)).toBe(true);
+			})
+		})
 	})
 })
