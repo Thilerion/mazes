8ad2e2ee7a1c5e8ae1002a125ae14b57cc6b3027
Michael van Meerwijk <michaelvanm@gmail.com>
2019-08-09 12:44:19 +0200

    Remove unvisited array from huntandkill

--- src/generators/huntAndKill.js
+++ src/generators/huntAndKill.js
@@ -5,19 +5,13 @@ export default function* huntAndKillAlgorithm({ grid, config }, onCycle, onFinis
 	const g = grid.grid;
 	const { animateHunting = true } = config.generators.huntAndKill;
 
-	let unvisited = [];
-	grid.forEachCell((cell, x, y) => {
-		unvisited.push(cell);
-	})
-
 	let currentCell = rndElement2D(g);
 	currentCell.initialized = true;
-	unvisited = withoutElement(currentCell, unvisited);
 
 	yield onCycle({ current: [currentCell] });
 
 	while (currentCell) {
-		const unvisitedNeighbors = currentCell.neighborsList.filter(nb => unvisited.includes(nb));
+		const unvisitedNeighbors = currentCell.neighborsList.filter(nb => !nb.initialized);
 
 		if (unvisitedNeighbors.length > 0) {
 			const neighbor = rndElement(unvisitedNeighbors);
@@ -26,7 +20,6 @@ export default function* huntAndKillAlgorithm({ grid, config }, onCycle, onFinis
 			currentCell.link(dir);
 			currentCell = neighbor;
 			currentCell.initialized = true;
-			unvisited = withoutElement(currentCell, unvisited);
 
 			yield onCycle({ current: [currentCell] });
 		} else {
@@ -47,12 +40,12 @@ export default function* huntAndKillAlgorithm({ grid, config }, onCycle, onFinis
 						yield onCycle({ current: [cell] });
 					}
 
-					if (!unvisited.includes(cell)) {
+					if (cell.initialized) {
 						continue;
 					}
 					
 
-					const visitedNeighbors = cell.neighborsList.filter(nb => !unvisited.includes(nb));
+					const visitedNeighbors = cell.neighborsList.filter(nb => nb.initialized);
 
 					if (visitedNeighbors.length > 0) {
 						currentCell = cell;
@@ -62,8 +55,6 @@ export default function* huntAndKillAlgorithm({ grid, config }, onCycle, onFinis
 
 						currentCell.link(nbDir);
 						currentCell.initialized = true;
-						
-						unvisited = withoutElement(currentCell, unvisited);
 					}
 				}
 			}
