b4c1cd02ab33a5215fa02569df7e92d1702d8e07
Thilerion <michaelvanm@gmail.com>
2019-08-16 20:24:04 +0200

    Change how renderers are exported

---
+++ src/renderers/index.js
@@ -0,0 +1,4 @@
+export {default as canvasV1} from './Canvas';
+export {default as canvasV2} from './Canvas2';
+export {default as canvasV3} from './Canvas3';
+export {default as toString} from './String';
